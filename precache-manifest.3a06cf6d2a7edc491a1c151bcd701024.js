1bcbd83e16ff3278ac654c495e58e0edc2eb6917
Apex <apexof@gmail.com>
2018-12-14 16:39:30 +0300

    Updates

---
+++ precache-manifest.3a06cf6d2a7edc491a1c151bcd701024.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c6851d09d382ad8c4c8",
+    "url": "/redux-counter/static/js/runtime~main.8c6851d0.js"
+  },
+  {
+    "revision": "62e57335834f4b941612",
+    "url": "/redux-counter/static/js/main.62e57335.chunk.js"
+  },
+  {
+    "revision": "55fd294ae5fab669a142",
+    "url": "/redux-counter/static/js/1.55fd294a.chunk.js"
+  },
+  {
+    "revision": "bb5ab6112a3bd2b2691aab8d71da4f15",
+    "url": "/redux-counter/index.html"
+  }
+];
